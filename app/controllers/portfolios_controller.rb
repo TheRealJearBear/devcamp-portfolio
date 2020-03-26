9b9d0a2468ed271db3d100066b6de32bb5ee5bf9
Jared Blake <jaredmarshallblake@gmail.com>
2020-03-26 19:18:00 -0400

    integrated the destroy method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,10 +13,10 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created, dawg.' }
-        format.json { render :show, status: :created, location: @portfolio }
+        #format.json { render :show, status: :created, location: @portfolio }
       else
         format.html { render :new }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        #format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -31,15 +31,29 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-        format.json { render :show, status: :ok, location: @portfolio_item }
+        #format.json { render :show, status: :ok, location: @portfolio_item }
       else
         format.html { render :edit }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+        #format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    #perform the lookup for the record
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #destroy/delete the record
+    @portfolio_item.destroy
+
+    #redirect to portfolios_url
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was removed.' }
+      #format.json { head :no_content }
+    end
   end
 end
