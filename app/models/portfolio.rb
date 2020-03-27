f6aa5cf363822c6fa56315ae1eb2a443327780d3
Jared Blake <jaredmarshallblake@gmail.com>
2020-03-27 13:37:57 -0400

    implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :subtitle, :main_image, :thumb_image, :body
 
   def self.angular
@@ -9,8 +10,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     ##the ||= is necessary because an = would override the image later if you already how one uploaded in there.
   end
 end
