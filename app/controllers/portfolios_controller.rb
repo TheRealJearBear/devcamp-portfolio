b3b4570a85dec6e50d01f8bedaac4339a5428b7d
Jared Blake <jaredmarshallblake@gmail.com>
2020-03-29 19:18:54 -0400

    added some notes about handling errors and edit management (rescue blocks) in user.rb model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -48,7 +48,6 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    binding.pry
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,3 +14,17 @@ class User < ApplicationRecord
     self.name.split.last
   end
 end
+
+
+##example of when and why you would use a rescue block.  Getting info from a site API like facebook and for some reason their site might go down for 1 minute, but your site would give an error, but it would not be because of OUR site, but because of FACEBOOK's site.
+
+#def get_facebook_messages
+#  begin
+#    contacts_fb
+#    @messages = retrieves_messages
+#  rescue => e
+#    flash[:error] = "Error occured; contacting Facebook: #{e}"
+#  end
+#end
+
+#there are still better ways to handle errors. For instance, there is no way to detect a syntax error using above set-up
