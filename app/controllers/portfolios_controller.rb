0dd22d3f91c0c1cc8bb77adc7f4fcae2a295d2bd
Jared Blake <jaredmarshallblake@gmail.com>
2020-04-04 10:49:53 -0400

    updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
     #you could do @portfolio_items = Portfolio.includes(:author, :skill) if Portfolio was a JOIN table.
   end
 
