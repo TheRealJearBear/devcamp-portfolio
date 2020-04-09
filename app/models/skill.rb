8ca8035e5b95fbe5532efb4f99952f331bcc5c4c
Jared Blake <jaredmarshallblake@gmail.com>
2020-04-09 14:19:44 -0400

    implemented styles for image upload buttons and fixed image placeholders

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,3 @@
 class Skill < ApplicationRecord
-  include Placeholder
   validates_presence_of :title, :percent_utilized
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-    ##the ||= is necessary because an = would override the image later if you already how one uploaded in there.
-  end
 end
